4cd056f3e93f3f6885502cabb178e0e61f1da595
Sorin Chis <kish.sorin@yahoo.com>
2019-05-23 18:34:43 +0300

    added react scroll

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home/index";
+import Navigation from "./components/NavBar/Navigation";
 import About from "./components/About/About";
 function App() {
   return (
     <div>
       <Home />
+      <Navigation />
       <About />
     </div>
   );

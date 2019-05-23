e896fb52cc4dbf03c0545faf1da7bfe1f76286dc
Sorin Chis <kish.sorin@yahoo.com>
2019-05-23 16:40:40 +0300

    added layout on about me page

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home/index";
-
+import About from "./components/About/About";
 function App() {
   return (
     <div>
       <Home />
+      <About />
     </div>
   );
 }
--- portofolio/src/components/Particles/index.js
+++ portofolio/src/components/Particles/index.js
@@ -14,44 +14,27 @@ const PartEffect = () => {
             }
           },
           size: {
-            value: 3,
-            random: true,
-            anim: {
-              speed: 4,
-              size_min: 0.3
-            }
+            value: 10,
+            random: true
+          },
+          move: {
+            direction: "bottom",
+            out_mode: "out"
           },
           line_linked: {
             enable: false
-          },
-          move: {
-            random: true,
-            speed: 1,
-            direction: "top",
-            out_mode: "out"
           }
         },
         interactivity: {
           events: {
-            onhover: {
-              enable: true,
-              mode: "bubble"
-            },
             onclick: {
               enable: true,
-              mode: "repulse"
+              mode: "remove"
             }
           },
           modes: {
-            bubble: {
-              distance: 250,
-              duration: 2,
-              size: 0,
-              opacity: 0
-            },
-            repulse: {
-              distance: 400,
-              duration: 4
+            remove: {
+              particles_nb: 10
             }
           }
         }

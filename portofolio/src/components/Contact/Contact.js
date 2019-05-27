a739a58b8732758ce665f23d058cd28c646ac680
Sorin Chis <kish.sorin@yahoo.com>
2019-05-27 19:07:06 +0300

    added more style for the form

--- portofolio/src/components/Contact/Contact.js
+++ portofolio/src/components/Contact/Contact.js
@@ -29,7 +29,8 @@ class Contact extends Component {
     return (
       <Element name="Contact">
         <ContactWrapper>
-          <h4>Contact Info here</h4>
+          <h3>Contact Me</h3>
+          <h4>Interest in working thogether?</h4>
           <Form onSubmit={this.sendMessage}>
             <input
               placeholder="Name"
@@ -62,7 +63,11 @@ export default Contact;
 const ContactWrapper = styled.div`
   background: lightgreen;
   height: 100vh;
-  padding-top: 80px;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
   h4 {
     margin: 0;
   }
@@ -72,9 +77,21 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  border: 1px solid black;
+  width: 90%;
+  margin: 0 auto;
+  padding: 20px;
+  background: darkred;
   input {
     padding: 20px 30px;
     font-size: 20px;
+    margin: 10px;
+    border-radius: 10px;
+  }
+
+  button {
+    padding: 20px 30px;
+    width: 60%;
+    border-radius: 20px;
   }
 `;

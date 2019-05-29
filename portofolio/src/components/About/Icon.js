9c73e8f8330fa35a6049174ff6c7de266fd128ea
Sorin Chis <kish.sorin@yahoo.com>
2019-05-29 15:32:28 +0300

    added content in About Component

--- portofolio/src/components/About/Icon.js
+++ portofolio/src/components/About/Icon.js
@@ -20,9 +20,18 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 120px;
+  max-width: 150px;
   position: relative;
   z-index: -10;
+  p:nth-of-type(1) {
+    margin-top: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    
+  }
+  p:nth-of-type(2) {
+    margin: 0;
+  }
   i {
     font-size: 60px;
     color: darkkhaki;

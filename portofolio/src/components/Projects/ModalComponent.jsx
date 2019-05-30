7a641885f1a71681fa8fcce761e6a395e0a776d2
Sorin Chis <kish.sorin@yahoo.com>
2019-05-30 14:20:08 +0300

    added Modal and Project Card styles in separate file

--- portofolio/src/components/Projects/ModalComponent.jsx
+++ portofolio/src/components/Projects/ModalComponent.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactModal from "react-modal";
-import styled from "styled-components";
+import {
+  overlay,
+  content,
+  ModalContent,
+  SocialWrapper,
+  AWrapper,
+  Button
+} from "./ProjectStyles";
 
 const ModalComponent = ({
   isOpen,
@@ -36,85 +43,3 @@ const ModalComponent = ({
 };
 
 export default ModalComponent;
-
-const overlay = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  zIndex: 300
-};
-
-const content = {
-  position: "absolute",
-  maxWidth: "300px",
-  margin: "0 auto",
-  padding: "1rem 1rem",
-  borderRadius: "0.5rem",
-  border: "1px solid #161c24",
-  background: "darkred",
-  overflow: "auto",
-  WebkitOverflowScrolling: "touch",
-  outline: "none",
-  zIndex: "200"
-};
-
-const Button = styled.button`
-  margin-top: 1.5rem;
-  background: #212b38;
-  border: 0;
-  width: 100%;
-  height: 4rem;
-  border-radius: 0.3rem;
-  color: white;
-  font-size: 16px;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-  outline: none;
-  -khtml-user-select: none;
-  -o-user-select: none;
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  user-select: none;
-  &:hover {
-    background: #85bdbf;
-  }
-`;
-
-const ModalContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  a {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: white;
-    text-decoration: none;
-    img {
-      width: 200px;
-      height: 230px;
-    }
-
-    h4 {
-      margin: 10px;
-    }
-  }
-`;
-
-const SocialWrapper = styled.div`
-  display: flex;
-  text-align: center;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const AWrapper = styled.a`
-  font-size: 32px;
-  padding: 10px 20px;
-  border-radius: 50%;
-  margin: 0 20px;
-  box-shadow: 0 15px 10px #777;
-`;

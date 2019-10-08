41a46fa95000b535591fc95c4823ce5d457519ba
Sorin Chis <kish.sorin@yahoo.com>
2019-10-09 00:04:27 +0300

    refactor Home component with Hooks, added  scroll event

--- portofolio/src/components/Home/index.js
+++ portofolio/src/components/Home/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useContext, useEffect, useState } from "react";
 import { Element, scroller } from "react-scroll";
 import Particles from "../Particles/index";
 import Flip from "react-reveal/Flip";
+import Context from "../context";
 import {
   HomeWrapper,
   TextWrapper,
@@ -11,40 +12,68 @@ import {
   AWrapper
 } from "./HomeStyle";
 
-class Home extends Component {
-  scrollToAbout = () => {
+const Home = () => {
+  const [theposition, setPosition] = useState(0);
+
+  // componentDidMount() {
+  //   window.addEventListener("scroll", this.listenToScroll);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener("scroll", this.listenToScroll);
+  // }
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => {
+      window.removeEventListener("scroll", listenToScroll);
+    };
+  }, []);
+
+  const listenToScroll = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolled = winScroll / height;
+
+    setPosition(scrolled);
+  };
+  const scrollToAbout = () => {
     scroller.scrollTo("About", {
       duration: 500,
       delay: 1,
       smooth: true
     });
   };
-  render() {
-    return (
-      <Element name="Home">
-        <HomeWrapper>
-          <Particles />
-          <TextWrapper>
-            <Flip left>
-              <NameWrapper>Sorin Chis</NameWrapper>
-              <h2>Full Stack Developer</h2>
-            </Flip>
-            <ButtonWrapper onClick={this.scrollToAbout}>
-              Click For More
-            </ButtonWrapper>
-          </TextWrapper>
-          <SocialWrapper>
-            <AWrapper href="https://github.com/SorinC6" target="_blank">
-              <i className="fab fa-github" />
-            </AWrapper>
-            <AWrapper href="https://linkedin.com/in/sorinchis" target="_blank">
-              <i className="fab fa-linkedin" />
-            </AWrapper>
-          </SocialWrapper>
-        </HomeWrapper>
-      </Element>
-    );
-  }
-}
+
+  const { state, dispatch } = useContext(Context);
+
+  console.log(Number(theposition.toFixed(3)));
+  return (
+    <Element name="Home">
+      <HomeWrapper>
+        <Particles />
+        <TextWrapper>
+          <Flip left>
+            <NameWrapper>Sorin Chis</NameWrapper>
+            <h2>Full Stack Developer</h2>
+          </Flip>
+          <ButtonWrapper onClick={scrollToAbout}>Click For More</ButtonWrapper>
+        </TextWrapper>
+        <SocialWrapper>
+          <AWrapper href="https://github.com/SorinC6" target="_blank">
+            <i className="fab fa-github" />
+          </AWrapper>
+          <AWrapper href="https://linkedin.com/in/sorinchis" target="_blank">
+            <i className="fab fa-linkedin" />
+          </AWrapper>
+        </SocialWrapper>
+      </HomeWrapper>
+    </Element>
+  );
+};
 
 export default Home;
